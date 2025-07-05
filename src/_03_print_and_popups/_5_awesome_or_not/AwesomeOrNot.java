a9b49b7a56f39474b44dc8c77220cce9337acfd3
league <league@imac-16.attlocal.net>
2025-07-05 11:30:16 -0700

    Working on ScaryMaze

--- src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
+++ src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
@@ -3,6 +3,8 @@ package _03_print_and_popups._5_awesome_or_not;
 import java.util.Date;
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes everything below
@@ -15,25 +17,31 @@ public class AwesomeOrNot {
 		// 2. Make a variable that will hold a random whole number
 	
 		// 3. Set your variable equal to a positive number less than 4 using     ran.nextInt(); 
-	
+		int value = ran.nextInt(4);
 		// 3. Print your variable to the console
-	
+		System.out.println(value);
 		// 4. Get the user to enter something that they think is awesome
-	
+		String name = JOptionPane.showInputDialog(null,"Enter something that you think is awesome");
 		// 5. If your variable is  0
-	
+		if(value == 0) {
+			JOptionPane.showMessageDialog(null,"That is awesome");
+		}
 			// -- tell the user whatever they entered is awesome!
 	
 		// 6. If your variable is  1
-	
+		if (value == 1) {
 			// -- tell the user whatever they entered is ok.
-	
+			JOptionPane.showMessageDialog(null,"That is ok");
+		}
 		// 7. If your variable is  2
-	
+		if (value == 2) {
+			JOptionPane.showMessageDialog(null, "That is boring");
 			// -- tell the user whatever they entered is boring.
-	
+		}
 		// 8. If your variable is  3
-	
+		if (value == 3) {
+			JOptionPane.showMessageDialog(null, "That is interesting");
+		}
 			// -- invent your own message to give to the user (be nice).
 	}
 }
